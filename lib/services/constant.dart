d5bf1b349034562981f589dc6baa7e67c54df749
vipul jain <vipul@pietechsolutiom.com>
2019-12-30 14:34:53 +0530

    change baseurl

--- lib/services/constant.dart
+++ lib/services/constant.dart
@@ -7,5 +7,5 @@ const ONE_SIGNAL_APP_ID = '05ac6c51-8416-41ab-8779-b48503bbe125';
 //api urls
 // const BASE_URL = 'http://192.168.0.5:7020/';
 
-const BASE_URL = 'https://restaurantsass.herokuapp.com/';
+const BASE_URL = 'https://restaurant-sass.herokuapp.com/';
 const API_ENDPOINT = BASE_URL + 'api/';
